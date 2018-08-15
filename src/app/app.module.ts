9907fecd796cd8167f84ea568228ac580c324c27
maceuge <maceuge@icloud.com>
2018-08-15 17:59:56 -0300

    Estructura inicial de la app, componentes, servicios y modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgresComponent } from './pages/progres/progres.component';
+import { Graphic1Component } from './pages/graphic1/graphic1.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SharedService } from './services/shared.service';
+import { SidebarService } from './services/sidebar.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgresComponent,
+    Graphic1Component,
+    NavbarComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SharedService,
+    SidebarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
