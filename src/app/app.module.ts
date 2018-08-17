0f8fc52a9386c0795b778526dc0e3a55549804fb
maceuge <maceuge@icloud.com>
2018-08-17 12:38:42 -0300

    Creacion de sitema modular y rutas modulares

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgresComponent } from './pages/progres/progres.component';
-import { Graphic1Component } from './pages/graphic1/graphic1.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
+// Servicios
 import { SharedService } from './services/shared.service';
 import { SidebarService } from './services/sidebar.service';
-import { AppRoutingModule } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { APP_ROOT } from '../../node_modules/@angular/core/src/di/scope';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgresComponent,
-    Graphic1Component,
-    NavbarComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PagesModule,
+    APP_ROUTES,
+    
   ],
   providers: [
     SharedService,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,13 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { Graphic1Component } from './pages/graphic1/graphic1.component';
-import { ProgresComponent } from './pages/progres/progres.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-    { path: '', component: PagesComponent, children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'graphic1', component: Graphic1Component},
-        { path: 'progres', component: ProgresComponent},
-        { path: '', pathMatch: 'full',  redirectTo: '/dashboard' }
-    ]},
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: '**', component: NopagefoundComponent },
 ];
 
-@NgModule({
-    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
-    exports: [RouterModule]
-})
-export class AppRoutingModule {}
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true});
