9ada1bbffbe605917529cad386ee09a317ae2fad
maceuge <maceuge@icloud.com>
2018-08-29 22:12:21 -0300

    Servicio de Usuario y register funcionando

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import swal from 'sweetalert';
 import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+import { Router } from '@angular/router';
 
 declare function init_plugin();
 
@@ -15,7 +17,8 @@ export class RegisterComponent implements OnInit {
   forma: FormGroup;
   pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
 
-  constructor(private userService: UsuarioService) { }
+  constructor(private userService: UsuarioService,
+              private _router: Router) { }
   
   ngOnInit() {
     init_plugin();
@@ -31,8 +34,8 @@ export class RegisterComponent implements OnInit {
     });
 
     this.forma.setValue({
-      nombre: 'Eugenio',
-      correo: 'james@gmail.com',
+      nombre: 'User',
+      correo: 'user@gmail.com',
       pass: '123456',
       pass2: '123456',
       termino: false
@@ -55,19 +58,27 @@ export class RegisterComponent implements OnInit {
   }
 
   guardarUsuario () {
-    
     if (this.forma.invalid) {
       return;
     }
-
     if (!this.forma.value.termino) {
       swal('No Aceptaste los Terminos', 'Tenes que aceptar los terminos para porder registrarte.', 'warning');
       return;
-    } 
+    }
+    
+    let usuario = new Usuario(
+      this.forma.value.nombre,
+      this.forma.value.correo,
+      this.forma.value.pass
+    );
 
-      console.log(this.forma.value);
-      console.log('Estado Forma: ', this.forma.valid);
-      console.log(this.forma.errors);
+    this.userService.crearUsuario(usuario).subscribe( data => {
+      this._router.navigate(['/login']);
+    });
+
+      //console.log(this.forma.value);
+      //console.log('Estado Forma: ', this.forma.valid);
+      //console.log(this.forma.errors);
   
   }
 
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICE } from '../../config/config';
-
+import { Usuario } from '../../models/usuario.model';
+import { map } from 'rxjs/operators';
+import swal from 'sweetalert';
 
 
 @Injectable({
@@ -12,7 +14,16 @@ export class UsuarioService {
   constructor(public _http: HttpClient) { 
     console.log('Soy el servicio del USUARIO');
 
-
-
   }
+
+  crearUsuario ( usuario: Usuario ) {
+    let url = URL_SERVICE + '/usuario';
+    return this._http.post( url, usuario).pipe(
+        map ( (data: any) => {
+          swal('Usuario Creado', usuario.email, 'success');
+          return data.usuario;
+        })
+    );
+  }
+
 }
